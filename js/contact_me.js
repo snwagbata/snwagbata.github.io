5ebe9d3b02dbf7ef444b7d8c736e567a347e9432
healer2030 <healer2030@gmail.com>
2018-03-30 22:04:57 -0400

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/healer2030@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
